688d9fc77438e7eda37bb15fdd35f9b183554369
oznuren <95101906+oznuren@users.noreply.github.com>
2022-06-24 12:43:49 +0300

    Added the ratings

--- src/routes/ProductDetail.jsx
+++ src/routes/ProductDetail.jsx
@@ -13,11 +13,14 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import Rating from "@mui/material/Rating";
 
 export default function ProductDetail({ products }) {
   const { productId } = useParams();
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [rates, setRates] = useState(null);
+  const [count, setCount] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const amnt = (min, max) => (v) => v <= min ? min : v >= max ? max : v;
@@ -26,6 +29,7 @@ export default function ProductDetail({ products }) {
 
   useEffect(() => {
     const getData = async () => {
+      setIsLoading(true);
       try {
         const response = await fetch(
           `https://fakestoreapi.com/products/${productId}`
@@ -35,6 +39,7 @@ export default function ProductDetail({ products }) {
             `This is an HTTP error: The status is ${response.status}`
           );
         }
+        setIsLoading(false);
         let actualData = await response.json();
         console.log(actualData);
         setData(actualData);
@@ -43,7 +48,7 @@ export default function ProductDetail({ products }) {
         setError(err.message);
         setData(null);
       } finally {
-        setLoading(false);
+        setIsLoading(false);
       }
     };
     getData();
@@ -72,108 +77,130 @@ export default function ProductDetail({ products }) {
         })
       );
   }
-  console.log(productId);
+  // console.log(productId);
+
+  // console.log(data);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setRates(data.rating.rate);
+      setCount(data.rating.count);
+    }, 1000);
+  });
 
   return (
-    <Card
-      sx={{
-        display: "flex",
-        border: "none",
-        boxShadow: "none",
-        justifyContent: "center",
-        marginTop: "50px",
-        marginTop: "50px",
-      }}
-    >
-      <Box sx={{ display: "flex", flexDirection: "column", width: "500px" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="h5">
-            {data.title}
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="text.secondary"
-            component="div"
-          >
-            {data.category}
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            color="text.secondary"
-            component="div"
-            fontSize="30px"
-          >
-            ${data.price}
-          </Typography>
+    <React.Fragment>
+      {error && <p>Something went wrong.</p>}
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <Card
+          sx={{
+            display: "flex",
+            border: "none",
+            boxShadow: "none",
+            justifyContent: "center",
+            marginTop: "50px",
+            marginTop: "50px",
+          }}
+        >
           <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              pl: 1,
-              pb: 1,
-              marginRight: "50px",
-              marginTop: "50px",
-            }}
+            sx={{ display: "flex", flexDirection: "column", width: "500px" }}
           >
-            {data.description}
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <Typography component="div" variant="h5">
+                {data.title}
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                component="div"
+              >
+                {data.category}
+              </Typography>
+              <Typography
+                variant="subtitle2"
+                color="text.secondary"
+                component="div"
+                fontSize="30px"
+              >
+                ${data.price}
+              </Typography>
+              <Box>
+                <Rating name="read-only" value={rates} readOnly />
+                <Typography component="legend">Out of {count} rates</Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  pl: 1,
+                  pb: 1,
+                  marginRight: "50px",
+                  marginTop: "50px",
+                }}
+              >
+                {data.description}
+              </Box>
+            </CardContent>
+            <Box display="flex" margin="10px">
+              <IconButton
+                sx={{
+                  width: "8%",
+                  borderRadius: 0,
+                  backgroundColor: "lightblue",
+                }}
+                onClick={() => setAmount(amntV(amount - 1))}
+              >
+                <RemoveIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                sx={{
+                  border: `1px solid lightblue`,
+                  p: 1,
+                  fontSize: "16px",
+                }}
+              >
+                {amount}
+              </Typography>
+              <IconButton
+                sx={{
+                  width: "8%",
+                  borderRadius: 0,
+                  backgroundColor: "lightblue",
+                }}
+                onClick={() => setAmount(amntV(amount + 1))}
+              >
+                <AddIcon />
+              </IconButton>
+            </Box>
+            <CardActions>
+              <Button
+                variant="contained"
+                aria-label="add to cart"
+                margin="10px"
+                onClick={handleClick}
+              >
+                <AddShoppingCartIcon sx={{ padding: "3px" }} />
+                Add to Cart
+              </Button>
+            </CardActions>
           </Box>
-        </CardContent>
-        <Box display="flex" margin="10px">
-          <IconButton
+          <CardMedia
+            component="img"
             sx={{
-              width: "8%",
-              borderRadius: 0,
-              backgroundColor: "lightblue",
+              width: "100%",
+              display: "flex",
+              justifyContent: "flex-end",
+              borderRadius: "5px",
+              maxWidth: "400px",
             }}
-            onClick={() => setAmount(amntV(amount - 1))}
-          >
-            <RemoveIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            sx={{
-              border: `1px solid lightblue`,
-              p: 1,
-              fontSize: "16px",
-            }}
-          >
-            {amount}
-          </Typography>
-          <IconButton
-            sx={{
-              width: "8%",
-              borderRadius: 0,
-              backgroundColor: "lightblue",
-            }}
-            onClick={() => setAmount(amntV(amount + 1))}
-          >
-            <AddIcon />
-          </IconButton>
-        </Box>
-        <CardActions>
-          <Button
-            variant="contained"
-            aria-label="add to cart"
-            margin="10px"
-            onClick={handleClick}
-          >
-            <AddShoppingCartIcon sx={{ padding: "3px" }} />
-            Add to Cart
-          </Button>
-        </CardActions>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "flex-end",
-          borderRadius: "5px",
-          maxWidth: "400px",
-        }}
-        image={`${data.image}`}
-        alt="No image"
-      />
-    </Card>
+            image={`${data.image}`}
+            alt="No image"
+          />
+        </Card>
+      )}
+    </React.Fragment>
   );
 }
